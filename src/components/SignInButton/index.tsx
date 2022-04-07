9ce2ae0883b8b823d03592911d3ff262f4debe28
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-04-07 11:46:05 -0300

    Session controled by next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,20 @@
 import styles from './styles.module.scss';
 import { FaGithub} from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
+    const { data: session } = useSession();
 
-    const isUserLoggeIn = true;
-
-    return isUserLoggeIn ? (
-        <button className={styles.singInButton} type="button">
+    console.log(session)
+    return session ? (
+        <button className={styles.singInButton} type="button" onClick={() => signOut()}>
             <FaGithub color='#04d361'/>
-            Emanuel Moura
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
-        <button className={styles.singInButton} type="button">
+        <button className={styles.singInButton} type="button" onClick={() => signIn('github')}>
             <FaGithub color='#eba417'/>
             Sign in with Github
         </button>
